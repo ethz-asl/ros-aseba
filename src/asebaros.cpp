2fa283ba7cf57e8bdc5602196c6fdf94bc2ef59d
Stéphane Magnenat <stephane@magnenat.net>
2010-10-11 16:22:05 +0200

    Rearchitectured, now each known event has its own topic, which matches well the ROS semantics. Compiles, but most functions not implemented.

--- src/asebaros.cpp
+++ src/asebaros.cpp
@@ -1,101 +1,388 @@
 #include "asebaros.h"
 
+#include <compiler/compiler.h>
+
+#include <ros/console.h>
+
 #include <vector>
+#include <sstream>
 #include <boost/format.hpp>
+
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+
 
 using namespace asebaros;
 using namespace std;
 using namespace boost;
+using namespace Dashel;
+using namespace Aseba;
 
+// AsebaDashelHub
 
-bool EventFilter::listenEventById(ListenEventById::Request& req, ListenEventById::Response& res)
+AsebaDashelHub::AsebaDashelHub(AsebaROS* asebaROS, unsigned port, bool forward):
+	Dashel::Hub(),
+	asebaROS(asebaROS),
+	forward(forward)
 {
-	return true;
+	ostringstream oss;
+	oss << "tcpin:port=" << port;
+	Dashel::Hub::connect(oss.str());
 }
 
-bool EventFilter::listenEventByName(ListenEventByName::Request& req, ListenEventByName::Response& res)
+void AsebaDashelHub::sendMessage(Message *message, Stream* sourceStream)
 {
-	return true;
+	// dump if requested
+	// TODO: check verbosity
+	// if (
+	ostringstream oss;
+	message->dump(oss);
+	ROS_DEBUG_STREAM(oss.str());
+	
+	// Might be called from the ROS thread, not the Hub thread, need to lock	
+	lock();
+
+	// write on all connected streams
+	for (StreamsSet::iterator it = dataStreams.begin(); it != dataStreams.end();++it)
+	{
+		Stream* destStream(*it);
+		
+		if ((forward) && (destStream == sourceStream))
+			continue;
+		
+		try
+		{
+			message->serialize(destStream);
+			destStream->flush();
+		}
+		catch (DashelException e)
+		{
+			// if this stream has a problem, ignore it for now, and let Hub call connectionClosed later.
+			ROS_ERROR("error while writing message");
+		}
+	}
+
+	unlock();
 }
 
-bool EventFilter::ignoreEventById(IgnoreEventById::Request& req, IgnoreEventById::Response& res)
+// the following method run in the blocking reception thread
+	
+void AsebaDashelHub::incomingData(Stream *stream)
 {
-	return true;
+	// receive message
+	Message *message = 0;
+	try
+	{
+		message = Message::receive(stream);
+	}
+	catch (DashelException e)
+	{
+		// if this stream has a problem, ignore it for now, and let Hub call connectionClosed later.
+		ROS_ERROR("error while writing message");
+	}
+	
+	// send message to Dashel peers
+	sendMessage(message, stream);
+	
+	// process message for ROS peers, the receiver will delete it
+	asebaROS->processAsebaMessage(message);
+	
+	// free the message
+	delete message;
 }
 
-bool EventFilter::ignoreEventByName(IgnoreEventByName::Request& req, IgnoreEventByName::Response& res)
+void AsebaDashelHub::connectionCreated(Stream *stream)
 {
-	return true;
+	ROS_INFO_STREAM("Incoming connection from " << stream->getTargetName());
+	
+	if (dataStreams.size() == 1)
+	{
+		// TODO: do somthing
+		//emit firstConnectionCreated();
+	}
 }
 
-bool EventFilter::freeEventFilter(FreeEventFilter::Request& req, FreeEventFilter::Response& res)
+void AsebaDashelHub::connectionClosed(Stream* stream, bool abnormal)
 {
-	return true;
-}
-
-EventFilter::EventFilter(unsigned number):
-	n(str(format("event_filter_%1%") % number))
-{
-	s.push_back(n.advertiseService("listen_event_by_id", &EventFilter::listenEventById, this));
-	s.push_back(n.advertiseService("listen_event_by_name", &EventFilter::listenEventByName, this));
-	s.push_back(n.advertiseService("ignore_event_by_id", &EventFilter::ignoreEventById, this));
-	s.push_back(n.advertiseService("ignore_event_by_name", &EventFilter::ignoreEventByName, this));
-	s.push_back(n.advertiseService("free", &EventFilter::freeEventFilter, this));
+	if (abnormal)
+		ROS_INFO_STREAM("Abnormal connection closed to " << stream->getTargetName() << " : " << stream->getFailReason());
+	else
+		ROS_INFO_STREAM("Normal connection closed to " << stream->getTargetName());
 }
 
 
+// AsebaROS
 
 bool AsebaROS::loadScript(LoadScripts::Request& req, LoadScripts::Response& res)
 {
+	// open document
+	const string& fileName(req.fileName);
+	xmlDoc *doc = xmlReadFile(fileName.c_str(), NULL, 0);
+	if (!doc)
+	{
+		ROS_ERROR_STREAM("Cannot read XML from file " << fileName);
+		return false;
+	}
+    xmlNode *domRoot = xmlDocGetRootElement(doc);
+	
+	// clear existing data
+	commonDefinitions.events.clear();
+	commonDefinitions.constants.clear();
+	userDefinedVariablesMap.clear();
+	pubs.clear();
+	subs.clear();
+	
+	// load new data
+	int noNodeCount = 0;
+	bool wasError = false;
+	for (xmlNode *domNode = domRoot; domNode; domNode = domNode->next)
+	{
+		if (domNode->type == XML_ELEMENT_NODE)
+		{
+			if (xmlStrEqual(domNode->name, BAD_CAST("node")))
+			{
+				// get attributes, child and content
+				xmlChar *name = xmlGetProp(domNode, BAD_CAST("name"));
+				if (!name)
+					ROS_WARN("missing \"name\" attribute in \"node\" entry");
+				xmlNode* child = xmlFirstElementChild(domNode);
+				if (!child)
+					ROS_WARN("missing child in \"node\" entry");
+				else if (name)
+				{
+					const string _name((const char *)name);
+					xmlChar * text = xmlNodeGetContent(child);
+					if (!text)
+						ROS_WARN("missing text in child of \"node\" entry");
+					else
+					{
+						bool ok;
+						unsigned nodeId(DescriptionsManager::getNodeId(_name, &ok));
+						if (ok)
+						{
+							// compile code
+							std::istringstream is((const char *)text);
+							Error error;
+							BytecodeVector bytecode;
+							unsigned allocatedVariablesCount;
+							
+							Compiler compiler;
+							compiler.setTargetDescription(getDescription(nodeId));
+							compiler.setCommonDefinitions(&commonDefinitions);
+							bool result = compiler.compile(is, bytecode, allocatedVariablesCount, error);
+							
+							if (result)
+							{
+								typedef std::vector<Message*> MessageVector;
+								MessageVector messages;
+								sendBytecode(messages, nodeId, std::vector<uint16>(bytecode.begin(), bytecode.end()));
+								for (MessageVector::const_iterator it = messages.begin(); it != messages.end(); ++it)
+								{
+									hub.sendMessage(*it);
+									delete *it;
+								}
+								Run msg(nodeId);
+								hub.sendMessage(&msg);
+								// retrieve user-defined variables for use in get/set
+								userDefinedVariablesMap[_name] = *compiler.getVariablesMap();
+							}
+							else
+							{
+								ROS_ERROR_STREAM("compilation of " << fileName << ", node " << _name << " failed: " << error.toString());
+								wasError = true;
+							}
+						}
+						else
+							noNodeCount++;
+						// free attribute and content
+						xmlFree(text);
+					}
+				}
+				if (name)
+					xmlFree(name);
+				
+			}
+			else if (xmlStrEqual(domNode->name, BAD_CAST("event")))
+			{
+				// get attributes
+				xmlChar *name = xmlGetProp(domNode, BAD_CAST("name"));
+				if (!name)
+					ROS_WARN("missing \"name\" attribute in \"event\" entry");
+				xmlChar *size = xmlGetProp(domNode, BAD_CAST("size")); 
+				if (!size)
+					ROS_WARN("missing \"size\" attribute in \"event\" entry");
+				// add event
+				if (name && size)
+				{
+					int eventSize(atoi((const char *)size));
+					if (eventSize > ASEBA_MAX_EVENT_ARG_SIZE)
+					{
+						ROS_ERROR("Event %s has a length %d larger than maximum %d", name, eventSize, ASEBA_MAX_EVENT_ARG_SIZE);
+						wasError = true;
+						break;
+					}
+					else
+						commonDefinitions.events.push_back(NamedValue(string((const char *)name), eventSize));
+				}
+				// free attributes
+				if (name)
+					xmlFree(name);
+				if (size)
+					xmlFree(size);
+			}
+			else if (xmlStrEqual(domNode->name, BAD_CAST("constant")))
+			{
+				// get attributes
+				xmlChar *name = xmlGetProp(domNode, BAD_CAST("name"));
+				if (!name)
+					ROS_WARN("missing \"name\" attribute in \"constant\" entry");
+				xmlChar *value = xmlGetProp(domNode, BAD_CAST("value")); 
+				if (!value)
+					ROS_WARN("missing \"value\" attribute in \"constant\" entry");
+				// add constant if attributes are valid
+				if (name && value)
+					commonDefinitions.constants.push_back(NamedValue(string((const char *)name), atoi((const char *)value)));
+				// free attributes
+				if (name)
+					xmlFree(name);
+				if (value)
+					xmlFree(value);
+			}
+			else
+				ROS_WARN_STREAM("Unknown XML node seen in .aesl file: " << domNode->name);
+		}
+	}
+
+	// release memory
+	xmlFreeDoc(doc);
+	
+	// check if there was an error
+	if (wasError)
+	{
+		ROS_ERROR_STREAM("There was an error while loading script " << fileName);
+		commonDefinitions.events.clear();
+		commonDefinitions.constants.clear();
+		userDefinedVariablesMap.clear();
+	}
+	
+	// check if there was some matching problem
+	if (noNodeCount)
+	{
+		ROS_WARN_STREAM(noNodeCount << " scripts have no corresponding nodes in the current network and have not been loaded.");
+	}
+	
+	// recreate publishers and subscribers
+	typedef EventsDescriptionsVector::const_iterator EventsDescriptionsConstIt;
+	for (EventsDescriptionsConstIt it(commonDefinitions.events.begin()); it != commonDefinitions.events.end(); ++it)
+	{
+		pubs.push_back(n.advertise<AsebaEvent>("events/"+it->name, 100));
+		subs.push_back(n.subscribe<AsebaEvent>("events/"+it->name, 100, bind(&AsebaROS::eventReceived, this, it->value, _1)));
+	}
+	
 	return true;
 }
 
 bool AsebaROS::getNodeList(GetNodeList::Request& req, GetNodeList::Response& res)
 {
+	//res.
+	// TODO
 	return true;
 }
 
 bool AsebaROS::getNodeId(GetNodeId::Request& req, GetNodeId::Response& res)
 {
+	// TODO
 	return true;
 }
 
 bool AsebaROS::getNodeName(GetNodeName::Request& req, GetNodeName::Response& res)
 {
+	// TODO
 	return true;
 }
 
 bool AsebaROS::getVariableList(GetVariableList::Request& req, GetVariableList::Response& res)
 {
+	// TODO
 	return true;
 }
 
 bool AsebaROS::setVariable(SetVariable::Request& req, SetVariable::Response& res)
 {
+	// TODO
 	return true;
 }
 
 bool AsebaROS::getVariable(GetVariable::Request& req, GetVariable::Response& res)
 {
+	// TODO
 	return true;
 }
 
-bool AsebaROS::sendEventById(SendEventById::Request& req, SendEventById::Response& res)
+bool AsebaROS::getEventId(GetEventId::Request& req, GetEventId::Response& res)
 {
-	return true;
+	size_t id;
+	if (commonDefinitions.events.contains(req.name, &id))
+	{
+		res.id = id;
+		return true;
+	}
+	return false;
 }
 
-bool AsebaROS::sendEventByName(SendEventByName::Request& req, SendEventByName::Response& res)
+bool AsebaROS::getEventName(GetEventName::Request& req, GetEventName::Response& res)
 {
-	return true;
+	if (req.id < commonDefinitions.events.size())
+	{
+		res.name = commonDefinitions.events[req.id].name;
+		return true;
+	}
+	return false;
 }
 
-bool AsebaROS::createEventFilter(CreateEventFilter::Request& req, CreateEventFilter::Response& res)
+void AsebaROS::sendEventOnROS(const UserMessage* asebaMessage)
 {
-	return true;
+	if (asebaMessage->type < commonDefinitions.events.size())
+	{
+		// known, send on a named channel
+		shared_ptr<AsebaEvent> event(new AsebaEvent);
+		event->source = asebaMessage->source;
+		event->data = asebaMessage->data;
+		pubs[asebaMessage->type].publish(event);
+	}
+	else
+	{
+		// unknown, send on the anonymous channel
+		shared_ptr<AsebaAnonymousEvent> event(new AsebaAnonymousEvent);
+		event->source = asebaMessage->source;
+		event->type = asebaMessage->type;
+		event->data = asebaMessage->data;
+		anonPub.publish(event);
+	}
 }
 
-AsebaROS::AsebaROS()
+void AsebaROS::nodeDescriptionReceived(unsigned nodeId)
+{
+	nodesNames.at(nodesDescriptions.at(nodeId).name) = nodeId;
+}
+
+void AsebaROS::eventReceived(const AsebaAnonymousEventConstPtr& event)
+{
+	UserMessage userMessage(event->type, event->data);
+	hub.sendMessage(&userMessage);
+}
+
+void AsebaROS::eventReceived(const uint16 id, const AsebaEventConstPtr& event)
+{
+	UserMessage userMessage(id, event->data);
+	hub.sendMessage(&userMessage);
+}
+
+AsebaROS::AsebaROS(unsigned port, bool forward):
+	anonPub(n.advertise<AsebaAnonymousEvent>("anonymous_events", 100)),
+	anonSub(n.subscribe("anonymous_events", 100, &AsebaROS::eventReceived, this)),
+	spinner(8), // number of threads for ROS
+	hub(this, port, forward) // hub for dashel 
 {
 	// script
 	s.push_back(n.advertiseService("load_script", &AsebaROS::loadScript, this));
@@ -111,18 +398,123 @@ AsebaROS::AsebaROS()
 	s.push_back(n.advertiseService("get_variable", &AsebaROS::getVariable, this));
 	
 	// events
-	s.push_back(n.advertiseService("send_event_by_id", &AsebaROS::sendEventById, this));
-	s.push_back(n.advertiseService("send_event_by_name", &AsebaROS::sendEventByName, this));
-	s.push_back(n.advertiseService("create_event_filter", &AsebaROS::createEventFilter, this));
+	s.push_back(n.advertiseService("get_event_id", &AsebaROS::getEventId, this));
+	s.push_back(n.advertiseService("get_event_name", &AsebaROS::getEventName, this));
 	
-	eventFilters.push_back(EventFilter(34));
+	spinner.start();
+}
+
+AsebaROS::~AsebaROS()
+{
+	xmlCleanupParser();
+}
+
+void AsebaROS::run()
+{
+	hub.run();
+}
+
+void AsebaROS::processAsebaMessage(Message *message)
+{
+	// scan this message for nodes descriptions
+	DescriptionsManager::processMessage(message);
+	
+	// if user message, send to D-Bus as well
+	UserMessage *userMessage = dynamic_cast<UserMessage *>(message);
+	if (userMessage)
+		sendEventOnROS(userMessage);
+	
+	// if variables, check for pending answers
+	Variables *variables = dynamic_cast<Variables *>(message);
+	if (variables)
+	{
+		const unsigned nodeId(variables->source);
+		const unsigned pos(variables->start);
+		// TODO
+		/*for (RequestsList::iterator it = pendingReads.begin(); it != pendingReads.end(); ++it)
+		{
+			RequestData* request(*it);
+			if (request->nodeId == nodeId && request->pos == pos)
+			{
+				QDBusMessage &reply(request->reply);
+				Values values(fromAsebaVector(variables->variables));
+				reply << QVariant::fromValue(values);
+				DBusConnectionBus().send(reply);
+				delete request;
+				pendingReads.erase(it);
+				break;
+			}
+		}*/
+	}
+}
+
+// AsebaROS::sendEvent(const AsebaEvent& asebaEvent)
+// {
+// 	// event
+// 	// TODO
+// }
+
+//! Show usage
+void dumpHelp(std::ostream &stream, const char *programName)
+{
+	stream << "AsebaROS, connects aseba components together and with ROS, usage:\n";
+	stream << programName << " [options] [additional targets]*\n";
+	stream << "Options:\n";
+	stream << "-l, --loop      : makes the switch transmit messages back to the send, not only forward them.\n";
+	stream << "-p port         : listens to incoming connection on this port\n";
+	stream << "-h, --help      : shows this help\n";
+	stream << "ROS_OPTIONS     : see ROS documentation\n";
+	stream << "Additional targets are any valid Dashel targets." << std::endl;
 }
 
 int main(int argc, char *argv[])
 {
 	ros::init(argc, argv, "aseba");
-	AsebaROS asebaROS;	
-	ros::spin();
+	
+	unsigned port = ASEBA_DEFAULT_PORT;
+	bool forward = true;
+	std::vector<std::string> additionalTargets;
+	
+	int argCounter = 1;
+	
+	while (argCounter < argc)
+	{
+		const char *arg = argv[argCounter];
+		
+		if ((strcmp(arg, "-l") == 0) || (strcmp(arg, "--loop") == 0))
+		{
+			forward = false;
+		}
+		else if (strcmp(arg, "-p") == 0)
+		{
+			arg = argv[++argCounter];
+			port = atoi(arg);
+		}
+		else if ((strcmp(arg, "-h") == 0) || (strcmp(arg, "--help") == 0))
+		{
+			dumpHelp(std::cout, argv[0]);
+			return 0;
+		}
+		else
+		{
+			additionalTargets.push_back(argv[argCounter]);
+		}
+		argCounter++;
+	}
+	
+	AsebaROS asebaROS(port, forward);
+	
+	try
+	{
+		for (size_t i = 0; i < additionalTargets.size(); i++)
+			asebaROS.connectTarget(additionalTargets[i]);
+	}
+	catch(Dashel::DashelException e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
+	asebaROS.run();
 
 	return 0;
 }
--- src/asebaros.h
+++ src/asebaros.h
@@ -1,6 +1,11 @@
 #ifndef __ASEBA_ROS_H
 #define __ASEBA_ROS_H
 
+#include <dashel/dashel.h>
+
+#include <msg/msg.h>
+#include <msg/descriptions-manager.h>
+
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
@@ -11,50 +16,69 @@
 #include "asebaros/GetVariableList.h"
 #include "asebaros/SetVariable.h"
 #include "asebaros/GetVariable.h"
-#include "asebaros/SendEventById.h"
-#include "asebaros/SendEventByName.h"
-#include "asebaros/CreateEventFilter.h"
-
-#include "asebaros/ListenEventById.h"
-#include "asebaros/ListenEventByName.h"
-#include "asebaros/IgnoreEventById.h"
-#include "asebaros/IgnoreEventByName.h"
-#include "asebaros/FreeEventFilter.h"
+#include "asebaros/GetEventId.h"
+#include "asebaros/GetEventName.h"
 
 #include "asebaros/AsebaEvent.h"
+#include "asebaros/AsebaAnonymousEvent.h"
 
 #include <vector>
+
+
+class AsebaROS;
+
+class AsebaDashelHub: public Dashel::Hub
+{
+private:
+	AsebaROS* asebaROS; //!< pointer to aseba ROS
+	bool forward; //!< should we only forward messages instead of transmit them back to the sender
+	
+public:
+	/*! Creates the hub, listen to TCP on port, and creates a DBus interace.
+		@param port port on which to listen for incoming connections
+		@param forward should we only forward messages instead of transmit them back to the sender
+	*/
+	AsebaDashelHub(AsebaROS* asebaROS, unsigned port, bool forward);
+	
+	/*! Sends a message to Dashel peers.
+		Does not delete the message, should be called by the main thread.
+		@param message aseba message to send
+		@param sourceStream originate of the message, if from Dashel.
+	*/
+	void sendMessage(Aseba::Message *message, Dashel::Stream* sourceStream = 0);
+
+protected:
+	virtual void connectionCreated(Dashel::Stream *stream);
+	virtual void incomingData(Dashel::Stream *stream);
+	virtual void connectionClosed(Dashel::Stream *stream, bool abnormal);
+};
 
 using namespace asebaros;
 
 typedef std::vector<ros::ServiceServer> ServiceServers;
+typedef std::vector<ros::Publisher> Publishers;
+typedef std::vector<ros::Subscriber> Subscribers;
 
-class EventFilter
+class AsebaROS: public Aseba::DescriptionsManager
 {
 protected:
-	ros::NodeHandle n;
-	ServiceServers s;
+	typedef std::map<std::string, unsigned> NodesNamesMap;
+	typedef std::map<std::string, Aseba::Compiler::VariablesMap> UserDefinedVariablesMap;
+	
+	ros::NodeHandle n; //!< node handler of this class 
+	ServiceServers s; //!< all services of this class
+	
+	ros::Publisher anonPub; //!< anonymous publisher, for aseba events with no associated name
+	ros::Subscriber anonSub; //!< anonymous subscriber, for aseba events with no associated name
+	Publishers pubs; //!< publishers for known events
+	Subscribers subs; //!< subscribers for known events
 
-protected:
-	bool listenEventById(ListenEventById::Request& req, ListenEventById::Response& res);
-	bool listenEventByName(ListenEventByName::Request& req, ListenEventByName::Response& res);
-	bool ignoreEventById(IgnoreEventById::Request& req, IgnoreEventById::Response& res);
-	bool ignoreEventByName(IgnoreEventByName::Request& req, IgnoreEventByName::Response& res);
-	bool freeEventFilter(FreeEventFilter::Request& req, FreeEventFilter::Response& res);
+	ros::AsyncSpinner spinner; //!< threads to process ros queries
+	AsebaDashelHub hub; //!< hub is the network interface for dashel peers
 	
-public:
-	EventFilter(unsigned number);
-};
-
-class AsebaROS
-{
-protected:
-	typedef std::vector<EventFilter> EventFilters;
-	
-	ros::NodeHandle n;
-	ServiceServers s;
-	
-	EventFilters eventFilters;
+	Aseba::CommonDefinitions commonDefinitions; //!< description of aseba constants and events
+	NodesNamesMap nodesNames; //!< the name of all nodes
+	UserDefinedVariablesMap userDefinedVariablesMap; //!< the name of the user-defined variables
 	
 protected:
 	bool loadScript(LoadScripts::Request& req, LoadScripts::Response& res);
@@ -67,12 +91,29 @@ protected:
 	bool setVariable(SetVariable::Request& req, SetVariable::Response& res);
 	bool getVariable(GetVariable::Request& req, GetVariable::Response& res);
 	
-	bool sendEventById(SendEventById::Request& req, SendEventById::Response& res);
-	bool sendEventByName(SendEventByName::Request& req, SendEventByName::Response& res);
-	bool createEventFilter(CreateEventFilter::Request& req, CreateEventFilter::Response& res);
+	bool getEventId(GetEventId::Request& req, GetEventId::Response& res);
+	bool getEventName(GetEventName::Request& req, GetEventName::Response& res);
+	
+	void sendEventOnROS(const Aseba::UserMessage* asebaMessage);
+	
+	// callbacks
+	void nodeDescriptionReceived(unsigned nodeId);
+	void eventReceived(const AsebaAnonymousEventConstPtr& event);
+	void eventReceived(const uint16 id, const AsebaEventConstPtr& event);
 	
 public:
-	AsebaROS();
+	AsebaROS(unsigned port, bool forward);
+	~AsebaROS();
+	
+	void run();
+	
+	// lOCK ? TODO
+	
+	void processAsebaMessage(Aseba::Message *message);
+	
+	void connectTarget(const std::string& target) { hub.connect(target); }
 };
+
+
 
 #endif // __ASEBA_ROS_H
